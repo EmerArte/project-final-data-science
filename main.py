71b5ada606fdcd706fd36fe70672ab5baf189695
Emer Arteaga <arteagaemer@gmail.com>
2022-07-09 14:20:34 -0500

    Inicio de desarrollo del front

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-# This is a sample Python script.
-
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import streamlit as st
 import pandas as pd
 import datetime
@@ -13,11 +9,6 @@ import streamlit.components.v1 as html
 from  PIL import Image
 import numpy as np
 import io 
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 def grafica_cantidad_sobre_tiempo(df):
     df_grp = df.groupby(['Año'])['CANTIDAD'].sum().reset_index()
@@ -31,7 +22,6 @@ def cargar_datos():
 
 st.set_page_config(page_title='Violencia intrafamiliar', layout='wide')
 df=cargar_datos();
-col1, col2 = st.columns([1, 3])
 
 
 with st.sidebar:
@@ -39,48 +29,32 @@ with st.sidebar:
                          icons=['house', 'kanban', 'book','person lines fill'],
                          menu_icon="app-indicator", default_index=0,
                          styles={
-        "container": {"padding": "5!important", "background-color": "#fafafa"},
-        "icon": {"color": "orange", "font-size": "25px"}, 
-        "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
-        "nav-link-selected": {"background-color": "#02ab21"},
+        "container": {"padding": "5!important","font-family":"sans-serif","font-weight":"bold", "background-color":"#FFFFFF"},
+        "icon": {"color": "#023047", "font-size": "25px","font-family":"sans-serif", "font-weight":"bold","color:hover": "#eee"}, 
+        "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee","font-family":"sans-serif"},
+        "nav-link-selected": {"background-color": "#FB8500"},
     }
     )
 
 if choose == "Más detalles":
-    with col1:
+    with st.container():
         st.markdown('Hola, aqui va una info grafica')
-    with col2:
+        st.multiselect("Seleccione", ["HOLA"], default=None, key=None, help=None, on_change=None, args=None, kwargs=None,disabled=False)
         st.markdown('Hola, aqui va una info grafica')    
     
 elif choose == "Contacto":
-    with col1:              
-        st.markdown(""" <style> .font {
-        font-size:20px ; font-family: 'Cooper Black'; color: #FF9633;} 
-        </style> """, unsafe_allow_html=True)
+    with st.container():              
         st.markdown('<p class="font">Acerca de los creadores</p>', unsafe_allow_html=True)    
-    with col2:              
-       st.markdown(""" <style> .font {
-        font-size:20px ; font-family: 'Cooper Black'; color: #FF9633;} 
-        </style> """, unsafe_allow_html=True)
-    st.write("Somos unos estudiantes de ingeniería de sistemas que queremos que puedas conocernos y nos ayudes a mejorar, si tienes alguna duda o sugerencia puedes contactarnos en el siguiente correo: fktcg99@gmail.com, tambien puedes aportar a nuestro proyecto en github: https://github.com/Cgalvispadilla/project-final-data-science")    
+        st.write("Somos unos estudiantes de ingeniería de sistemas que queremos que puedas conocernos y nos ayudes a mejorar, si tienes alguna duda o sugerencia puedes contactarnos en el siguiente correo: fktcg99@gmail.com, tambien puedes aportar a nuestro proyecto en github: https://github.com/Cgalvispadilla/project-final-data-science")    
     
 
 elif choose == "Predecir":
-    with col1:
+    with st.container():
         st.markdown('Hola, aqui va una info grafica')
-    with col2:
         st.markdown('Hola, aquí va una info grafica')
 elif choose == "Home":
-    with col1:
-        st.markdown(""" <style> .font {
-        font-size:20px ; font-family: 'Cooper Black'; color: #FF9633;} 
-        </style> """, unsafe_allow_html=True)
-        st.markdown('<p class="font"> Conclusiones de la grafica iran aquí</p>', unsafe_allow_html=True)
-    with col2:
+    with st.container():
         st.title('Hola, bienvenido a la plataforma de predicción de violencia intrafamiliar')
-        st.markdown(""" <style> .font {
-        font-size:20px ; font-family: 'Cooper Black'; color: #000000;} 
-        </style> """, unsafe_allow_html=True)
         st.plotly_chart(grafica_cantidad_sobre_tiempo(df), use_container_width=False)            
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
