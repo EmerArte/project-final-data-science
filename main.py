490d6299fec0db3e9bf596da5fe77223726d0af6
Emer Arteaga <arteagaemer@gmail.com>
2022-07-10 23:03:42 -0500

    primera entrega dashboard

--- main.py
+++ main.py
@@ -10,15 +10,28 @@ import numpy as np
 import io
 
 
-def graphy_case_anio(df):
-    df_grp = df.groupby(['Año'])['CANTIDAD'].sum().reset_index()
-    return px.bar(df_grp, x='Año', y="CANTIDAD", title='CANTIDAD DE CASOS DE VIOLENCIA INTRAFAMILIAR POR AÑO',
-    color_discrete_sequence=['#fb8501'])
-
+def graphy_serie_time(df):
+    df_grp = df.groupby(['FECHA HECHO'])['CANTIDAD'].sum().reset_index()
+    fig = px.line(df_grp, x='FECHA HECHO', y="CANTIDAD", title='CANTIDAD DE CASOS DE VIOLENCIA INTRAFAMILIAR POR AÑO',
+                  color_discrete_sequence=['#7BE583'])
+    fig.update_layout({'plot_bgcolor': 'rgba(0,0,0,0)', 'paper_bgcolor': 'rgba(0,0,0,0)'})
+    fig.update_xaxes(rangeslider_visible=True,
+                     rangeselector=dict(buttons=list(
+                         [
+                             dict(step='day', stepmode='backward', label='1 semana', count=7),
+                             dict(step='month', stepmode='backward', label='1 mes', count=1),
+                             dict(step='month', stepmode='backward', label='3 meses', count=3),
+                             dict(step='year', stepmode='backward', label='1 año', count=1),
+                             dict(label='Mostrar Todo', step='all')
+                         ]
+                     )))
+    return fig
 def graphy_case_depto(df):
     df_grp1 = df.groupby(['DEPARTAMENTO'])['CANTIDAD'].sum().reset_index()
-    fig = px.bar(df_grp1.sort_values(by='CANTIDAD', ascending=False), x='DEPARTAMENTO', y='CANTIDAD',
-                 title='CANTIDAD DE CASOS DE VIOLENCIA INTRAFAMILIAR POR DEPARTAMENTO')
+    fig = px.line(df_grp1.sort_values(by='CANTIDAD', ascending=False), x='DEPARTAMENTO', y='CANTIDAD',
+                 title='CANTIDAD DE CASOS DE VIOLENCIA INTRAFAMILIAR POR DEPARTAMENTO',
+                 color_discrete_sequence=['#7BE583'])
+    fig.update_layout({'plot_bgcolor': 'rgba(0,0,0,0)', 'paper_bgcolor': 'rgba(0,0,0,0)'})
     fig.update_xaxes(tickangle=-90)
     return fig
 
@@ -32,14 +45,62 @@ def graphy_case_top5_depto(df):
     fig = px.bar(df_grp7.sort_values(by='CANTIDAD', ascending=False), x="DEPARTAMENTO", y='CANTIDAD',
                  color="ARMAS MEDIOS",
                  title="NUMERO DE CASOS POR DÍA EN LOS 5 DEPARTAMENTOS CON MÁS VIOLENCIA INTRAFAMILIAR",
-                 barmode='group',
-                 color_discrete_map={"SIN EMPLEO DE ARMAS": "#273746",
-                                     "CONTUNDENTES": "#28B463",
-                                     "ARMA BLANCA / CORTOPUNZANTE": "#F1C40F",
-                                     "ESCOPOLAMINA": "#138D75",
-                                     "NO REPORTA": "#3498DB",
-                                     "ARMA DE FUEGO": "red"
+                  barmode='group',
+                 color_discrete_map={"SIN EMPLEO DE ARMAS": "#9EFFCA",
+                                     "CONTUNDENTES": "#26A18D",
+                                     "ARMA BLANCA / CORTOPUNZANTE": "#004E64",
+                                     "ESCOPOLAMINA": "#00A5CF",
+                                     "NO REPORTA": "#8ECAE6",
+                                     "ARMA DE FUEGO": "#3DECAF"
                                      })
+    fig.update_layout({'plot_bgcolor': 'rgba(0,0,0,0)', 'paper_bgcolor': 'rgba(0,0,0,0)'})
+    return fig
+
+
+def graphy_grupo_etario_top5_depto(df):
+    df_grp9 = df.groupby(['DEPARTAMENTO', 'GRUPO ETARIO'])['CANTIDAD'].sum().reset_index()
+    df_grp9 = df_grp9[(df_grp9['DEPARTAMENTO'] == 'CUNDINAMARCA') | (df_grp9['DEPARTAMENTO'] == 'ANTIOQUIA') | (
+                df_grp9['DEPARTAMENTO'] == 'VALLE') | (df_grp9['DEPARTAMENTO'] == 'SANTANDER') | (
+                                  df_grp9['DEPARTAMENTO'] == 'BOYACÁ')]
+
+    fig = px.bar(df_grp9.sort_values(by='CANTIDAD', ascending=True), x="GRUPO ETARIO", y='CANTIDAD',
+                 color="DEPARTAMENTO", title="CASOS POR GRUPO ETARIO - TOP 5 DEPARTAMENTOS MÁS VIOLENTOS",
+                  barmode='group',
+                 color_discrete_map={"CUNDINAMARCA": "#9EFFCA",
+                                     "ANTIOQUIA": "#00A5CF",
+                                     "VALLE": "#26A18D",
+                                     "SANTANDER": "#004E64",
+                                     "BOYACÁ": "#78E07C",
+                                     })
+    fig.update_layout({'plot_bgcolor': 'rgba(0,0,0,0)', 'paper_bgcolor': 'rgba(0,0,0,0)'})
+    return fig
+def graphy_depto_gender(df):
+    df_grp8 = df.groupby(['DEPARTAMENTO', 'ARMAS MEDIOS', 'GENERO'])['CANTIDAD'].sum().reset_index()
+    df_grp8 = df_grp8[(df_grp8['ARMAS MEDIOS'] == 'ARMA DE FUEGO')]
+    fig = px.bar(df_grp8.sort_values(by='CANTIDAD', ascending=False), x="DEPARTAMENTO", y="CANTIDAD", color="GENERO",
+                 title="DEPARTAMENTOS CON MAYOR USO DE ARMAS DE FUEGO POR GENERO",
+                 color_discrete_map={"FEMENINO": "#78E07C",
+                                     "MASCULINO": "#00A5CF",
+                                     })
+    fig.update_layout({'plot_bgcolor': 'rgba(0,0,0,0)', 'paper_bgcolor': 'rgba(0,0,0,0)'})
+    fig.update_xaxes(tickangle=-90)
+    return fig
+
+def graphy_day_of_week_depto(df):
+    df_grp5 = df.groupby(['DEPARTAMENTO', 'Dia de la semana'])['CANTIDAD'].sum().reset_index()
+    df_grp5 = df_grp5[(df_grp5['DEPARTAMENTO'] == 'CUNDINAMARCA') | (df_grp5['DEPARTAMENTO'] == 'ANTIOQUIA') | (
+                df_grp5['DEPARTAMENTO'] == 'VALLE') | (df_grp5['DEPARTAMENTO'] == 'SANTANDER') | (
+                                  df_grp5['DEPARTAMENTO'] == 'BOYACÁ')]
+    fig = px.bar(df_grp5.sort_values(by='CANTIDAD', ascending=False), x="Dia de la semana", y='CANTIDAD',
+                 color="DEPARTAMENTO",
+                 title="NUMERO DE CASOS POR DÍA EN LOS 5 DEPARTAMENTOS CON MÁS VIOLENCIA INTRAFAMILIAR", barmode='group',
+                 color_discrete_map={"CUNDINAMARCA": "#78E07C",
+                                     "ANTIOQUIA": "#004E64",
+                                     "VALLE": "#00A5CF",
+                                     "SANTANDER": "#26A18D",
+                                     "BOYACÁ": "#9EFFCA",
+                                     })
+    fig.update_layout({'plot_bgcolor': 'rgba(0,0,0,0)', 'paper_bgcolor': 'rgba(0,0,0,0)'})
     return fig
 @st.cache(allow_output_mutation=True)
 def cargar_datos():
@@ -87,8 +148,13 @@ if choose == "Home":
         </p>
         """, unsafe_allow_html=True)   
         col1, col2 = st.columns(2)
-        col1.plotly_chart(graphy_case_anio(df),
-                            use_container_width=False)
+
+        with col1:
+            st.plotly_chart(graphy_case_top5_depto(df), use_container_width=True)
+            st.plotly_chart(graphy_grupo_etario_top5_depto(df), use_container_width=True)
+        with col2:
+            st.plotly_chart(graphy_depto_gender(df), use_container_width=True)
+            st.plotly_chart(graphy_day_of_week_depto(df), use_container_width=True)
 
 elif choose == "Más detalles":
     with st.container():
