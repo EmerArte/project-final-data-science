ef88740640cf33fac3f22bf48e4b4023b5bdf35b
Emer Arteaga <48594349+EmerArte@users.noreply.github.com>
2022-07-16 09:02:04 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,5 +4,11 @@ echo "\
 headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
+[theme]\n\
+primaryColor = '#26A18D'\n\
+backgroundColor = '#FFFFFF'\n\
+secondaryBackgroundColor = '#F3F3F3'\n\
+textColor = '#023047'\n\
+font = 'sans serif'\n\
 \n\
 " > ~/.streamlit/config.toml
